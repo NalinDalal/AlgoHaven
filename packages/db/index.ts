3cf56dfdb8e5d41d075d3a48f7952ab2f73496a7
Nalin <nalindalal2004@gmail.com>
2026-03-16 19:11:50 +0530

    added judge

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg"; // or the adapter for your database
+
+export { JudgePhase, Role , SubmissionStatus } from './generated/prisma/client';
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
