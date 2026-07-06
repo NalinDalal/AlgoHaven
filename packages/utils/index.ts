f8d38540865a6e7a19b4fc1f20adbe00f9310e5c
Nalin Dalal <116961144+NalinDalal@users.noreply.github.com>
2026-07-06 10:26:48 +0000

    missing package export

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -3,6 +3,7 @@ export { withTimeout } from "./timeout";
 export { success, failure, createResponse } from "./response";
 export { matchRoute } from "./matchRoute";
 export { getRankTier, BADGES, type Badge, type BadgeStats } from "./badges";
+export { validateEnv } from "./env";
 export {
   type RouteParams,
   type IdParams,
